ab5c86c45f1c8ac1663b3c67838f9b46788f5a61
bruno <bbrancaglione26@gmail.com>
2018-09-26 16:27:03 -0300

    v1.0
    Primeiro Commit do Projeto

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string nomeProduto;
+                int quantidade;
+                double valorProduto, valorTotal;
+
+                Console.Write("Digite o nome do produto: ");
+                nomeProduto = Console.ReadLine();
+
+                Console.Write("Digite o valor do produto: ");
+                valorProduto = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Digite a quantidade");
+                quantidade = Convert.ToInt32(Console.ReadLine());
+
+                valorTotal = quantidade * valorProduto;
+
+                Console.Clear();
+                Console.WriteLine("**** Venda de Produtos*******");
+                Console.WriteLine("Produto: " + nomeProduto);
+                Console.WriteLine("Qtde: " + quantidade + " x valor Unitário: " + valorProduto.ToString("C"));
+                Console.WriteLine("total a Venda: " + valorTotal.ToString("C"));
+                //.toString()-> converte numero para Texto, o "C" -> formata para moeda.
+            }
+            catch (Exception)
+            {
+                Console.Clear();
+                Console.WriteLine("Dados Digitados Invalidos!");
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
